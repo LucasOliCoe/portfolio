915f4d7117ef23b6b6d2a2ea914de64830778c0f
Lucas Coelho <lucasolicontato@gmail.com>
2025-10-02 19:00:38 -0300

    Adiciona projeto de Pipeline Pipeline de dados: combinando Python e orientação a objeto

---
+++ Projeto  - Pipeline Orientação a objeto/fusao_mercado_fev.py
@@ -0,0 +1,36 @@
+from processamento_dados import Dados
+
+path_json = 'data_raw/dados_empresaA.json'
+path_csv = 'data_raw/dados_empresaB.csv'
+
+# Extract
+
+dados_empresaA = Dados(path_json, 'json')
+print(f'Nome das colunas base empresa A: {dados_empresaA.nome_colunas}')
+print(f'Quantidade de linhas base empresa A: {dados_empresaA.qtd_linhas}')
+
+dados_empresaB = Dados(path_csv, 'csv')
+print(f'Nome das colunas base empresa B: {dados_empresaB.nome_colunas}')
+print(f'Quantidade de linhas base empresa B: {dados_empresaB.qtd_linhas}')
+
+# Transform
+
+key_mapping = {'Nome do Item': 'Nome do Produto',
+                'Classificação do Produto': 'Categoria do Produto',
+                'Valor em Reais (R$)': 'Preço do Produto (R$)',
+                'Quantidade em Estoque': 'Quantidade em Estoque',
+                'Nome da Loja': 'Filial',
+                'Data da Venda': 'Data da Venda'}
+
+dados_empresaB.rename_columns(key_mapping)
+print(dados_empresaB.nome_colunas)
+
+dados_fusao = Dados.join(dados_empresaA, dados_empresaB)
+print(f'Nome das colunas base dados fusao: {dados_fusao.nome_colunas}')
+print(f'Quantidade de linhas base dados fusao: {dados_fusao.qtd_linhas}')
+
+# Load
+
+path_dados_combinados = 'data_processed/dados_combinados.csv'
+dados_fusao.salvando_dados(path_dados_combinados)
+print(path_dados_combinados)
